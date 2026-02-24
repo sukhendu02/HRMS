238208c9679455db1a844daa3e3dd91425e2ecff
Sukhendu <sukhendumandal02@gmail.com>
2026-02-24 14:24:22 +0530

    fixed cors

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,15 @@ import {connectDB} from './Config/db.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+import cors from "cors";
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://hrms-seven-dusky.vercel.app"
+  ],
+  credentials: true
+}));
 
 const startServer = async () => {
   await connectDB();
